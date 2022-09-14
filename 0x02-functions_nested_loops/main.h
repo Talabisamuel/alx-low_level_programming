6de5b88ef33d51bd6e71197b59efa454b4ece826
talabisamuel <talabisamuel4600@gmail.com>
2022-09-14 13:28:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,8 @@
-#include "main.h"
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INLUDED
 
-/**
- * main - print putchar
- * Return: always 0
- */
-
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isapha(int c);
